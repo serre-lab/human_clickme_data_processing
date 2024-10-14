69e94eb9e24f630acd52e84a0f7fb0326b805557
Drew Linsley <drewlinsley@gmail.com>
2024-10-14 17:29:45 -0400

    Fixed.

--- src/utils.py
+++ src/utils.py
@@ -219,6 +219,7 @@ def process_clickmap_files(
         file_exclusion_filter=None,
         process_max="trim"):
     clickmaps = {}
+    import pdb;pdb.set_trace()
     for _, row in clickme_data.iterrows():
         image_file_name = os.path.sep.join(row['image_path'].split(os.path.sep)[-2:])
         if file_inclusion_filter is not None and file_inclusion_filter not in image_file_name:
