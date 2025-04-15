0b358c60d6f87f8f2f212ff14a4233467551554b
Drew Linsley <drewlinsley@gmail.com>
2025-04-15 11:13:28 -0400

    Updated.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+# Define the extension
+extensions = [
+    Extension(
+        "src.cython_utils",
+        ["src/cython_utils.py"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"]  # Optimize for maximum performance
+    )
+]
+
+# Setup configuration
+setup(
+    name="clickme_processing",
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            "language_level": 3,
+            "boundscheck": False,
+            "wraparound": False,
+            "initializedcheck": False,
+        },
+    ),
+    include_dirs=[np.get_include()]
+) 
