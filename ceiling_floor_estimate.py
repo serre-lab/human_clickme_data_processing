d22e178922d50446fe6904c15b1833b30af0705d
Drew Linsley <drewlinsley@gmail.com>
2025-05-19 11:35:16 -0400

    Updated.

--- ceiling_floor_estimate.py
+++ ceiling_floor_estimate.py
@@ -434,6 +434,7 @@ if __name__ == "__main__":
     #     n_jobs = adjusted_n_jobs
     
     # Process correlation batches in parallel
+    n_jobs = 1
     ceiling_results = Parallel(n_jobs=n_jobs, prefer="threads")(
         delayed(compute_correlation_batch)(
             batch_indices=batch,
