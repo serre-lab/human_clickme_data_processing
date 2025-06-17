e30d57d2efacddce57d3ad5c6a7c72f7c258c332
PPPayson <peisenzhou98@gmail.com>
2025-06-17 16:01:27 -0400

    Fix click bins saving for batched processing

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# python prepare_clickmaps.py configs/co3d_val_oscar.yaml
+# python prepare_clickmaps.py configs/co3d_constancy_val_oscar.yaml
+# python prepare_clickmaps.py configs/co3d_train_oscar.yaml
+# python prepare_clickmaps.py configs/imagenet_co3d_val_oscar.yaml
+# python prepare_clickmaps.py configs/imagenet_train_oscar.yaml
+python prepare_clickmaps.py configs/imagenet_val_oscar.yaml
